65b7828a5885052bca564c587ede6b90ba81e5c0
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-10-04 17:13:17 +0530

    created a website with index.html and minor fxes

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
-const click = () =>{
-  chrome.tabs.executeScript(null,
-    {
-      code:"console.log('Welcome');"
-    }
-  );
-  window.close();
-
-}
-document.addEventListener('DOMContentLoaded', function(){
-  document.addEventListener('click',click);
-});
+// const click = () =>{
+//   chrome.tabs.executeScript(null,
+//     {
+//       code:"console.log('Welcome');"
+//     }
+//   );
+//   window.close();
+//
+// }
+// document.addEventListener('DOMContentLoaded', function(){
+//   document.addEventListener('click',click);
+// });
